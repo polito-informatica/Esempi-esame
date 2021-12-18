368e818534f7cd63bffb978f6a9f059ba72ffbd2
Fulvio Corno <fulvio.corno@polito.it>
2021-12-18 22:58:07 +0100

    Revisione testo e soluzione 'bowling'

--- esami/bowling/bowling.py
+++ esami/bowling/bowling.py
@@ -1,4 +1,3 @@
-from sys import exit
 from operator import itemgetter
 
 NOME_FILE = 'bowling.txt'
@@ -10,7 +9,7 @@ def main():
         infile = open(NOME_FILE, "r", encoding="utf-8")
         partita = leggi_file(infile)  # se posso aprire il file carico i dati
         visualizza(partita)
-    except IOError:
+    except OSError:
         print("Error: file bowling.txt not found.")
         exit(-1)
 
@@ -20,24 +19,21 @@ def main():
 def leggi_file(fp):
     partita = []
     for line in fp:
-        linea = line.rstrip()
-        campi = linea.split(";")
-        lista = []
-        totale = 0
-        n_max = 0
-        n_min = 0
-        for i in range(2, len(campi)):
-            lista.append(campi[i])
-            totale = totale + int(campi[i])
-            if int(campi[i]) == 10:
+        campi = line.rstrip().split(";")
+        punteggi = []
+        n_max = n_min = 0
+        for campo in campi[2:]:
+            num = int(campo)
+            punteggi.append(num)
+            if num == 10:
                 n_max = n_max + 1
-            if int(campi[i]) == 0:
+            elif num == 0:
                 n_min = n_min + 1
         record_giocatore = {  # creo un dizionario che contiene i dati di ogni giocatore
             'cognome': campi[0],
             'nome': campi[1],
-            'punteggi': lista,
-            'totale': totale,
+            'punteggi': punteggi,
+            'totale': sum(punteggi),
             'massimo': n_max,
             'minimo': n_min
         }
@@ -52,17 +48,19 @@ def visualizza(partita):
     piu_dieci = 0
     piu_zero = 0
     for i in range(0, len(partita_ordinata)):
-        print("%-20s %-20s %-3d" % (
-            partita_ordinata[i]["cognome"], partita_ordinata[i]["nome"], partita_ordinata[i]["totale"]))
-        if partita_ordinata[i]["minimo"] > partita_ordinata[piu_zero]["minimo"]:
+        p = partita_ordinata[i]
+        print(f'{p["cognome"]:20s} {p["nome"]:20s} {p["totale"]:3d}')
+        if p["minimo"] > partita_ordinata[piu_zero]["minimo"]:
             piu_zero = i
-        if partita_ordinata[i]["massimo"] > partita_ordinata[piu_dieci]["massimo"]:
+        if p["massimo"] > partita_ordinata[piu_dieci]["massimo"]:
             piu_dieci = i
+
     print("%s %s ha abbattuto tutti i birilli %d volta/e" % (
         partita_ordinata[piu_dieci]["cognome"], partita_ordinata[piu_dieci]["nome"],
         partita_ordinata[piu_dieci]["massimo"]))
     print("%s %s ha mancato tutti i birilli %d volta/e" % (
-        partita_ordinata[piu_zero]["cognome"], partita_ordinata[piu_zero]["nome"], partita_ordinata[piu_zero]["minimo"]))
+        partita_ordinata[piu_zero]["cognome"], partita_ordinata[piu_zero]["nome"],
+        partita_ordinata[piu_zero]["minimo"]))
     return
 
 
