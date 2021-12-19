a5eb3e20f1261f69713cc8fbc1fde384f733c964
Fulvio Corno <fulvio.corno@polito.it>
2021-12-19 19:23:22 +0100

    Revisione testo e soluzione 'hacking'

--- esami/hacking/hacking.py
+++ esami/hacking/hacking.py
@@ -1,17 +1,21 @@
-'''
-Struttura dati: un dizionario di prodotti
+"""
+Struttura dati: un dizionario di prodotti, la cui chiave è il codice prodotto
 
 Per ogni prodotto, un dizionario con due campi:
 - il rivenditore ufficiale
-- la lista di rivenditori segnalata dai clienti
-
-Nota: il rivenditore ufficiale è una lista, in modo da poter controllare
-in modo immediato se la lista dei rivenditori ufficiali è diversa dalla
-lista dei rivenditori attuali (con gli operatori di uguaglianza tra liste)
-'''
+- un insieme dei rivenditori segnalata dai clienti
+"""
 
 
 def venditori_ufficiali(filename):
+    """
+    Legge l'elenco dei rivenditori ufficiali. Restituisce un dizionario, con chiave uguale al codice prodotto
+    e valore pari ad un dizionario { ufficiale : codice venditore, rivenditori: set() }
+    Il campo 'rivenditori' sarà utile alla funzione leggi_acquisti, per ora viene inizializzato all'insieme vuoto.
+
+    :param filename: nome del file da leggere
+    :return: il dizionario contenente i prodotti ed i rivenditori ufficiali
+    """
     file = open(filename, 'r')
 
     venditori = {}
@@ -20,20 +24,28 @@ def venditori_ufficiali(filename):
         prodotto = parti[0]
         venditore = parti[1]
 
-        venditori[prodotto] = {'ufficiale': [venditore], 'rivenditori': []}
+        venditori[prodotto] = {'ufficiale': venditore, 'rivenditori': set()}
 
     file.close()
     return venditori
 
 
 def leggi_acquisti(venditori, filename):
+    """
+    Arricchisce il dizionario 'venditori' con l'elenco dei rivenditori effettivi, letti dal file.
+    Aggiunge dei codici di rivenditori all'insieme 'rivendtitori' del prodotto. Non restisuisce nulla, poiché
+    modifica direttamente 'venditori'.
+
+    :param venditori: dizionario con prodotti e rivenditori ufficiali
+    :param filename: nome del file contenente gli acquisti
+    """
     file = open(filename, 'r')
 
     for line in file:
         parti = line.strip().split()
         prodotto = parti[0]
         venditore = parti[1]
-        venditori[prodotto]['rivenditori'].append(venditore)
+        venditori[prodotto]['rivenditori'].add(venditore)
 
     file.close()
 
@@ -48,11 +60,11 @@ def main():
         rivenditori = venditori[prodotto]['rivenditori']
 
         # i prodotti non acquistati, non vanno considerati
-        # quelli che hanno una lista di rivenditori diversi dalla lista ufficiale
+        # quelli che hanno dei rivenditori diversi dalla lista ufficiale
         # vanno segnalati
-        if len(rivenditori) > 0 and ufficiale != rivenditori:
+        if len(rivenditori) > 0 and rivenditori != {ufficiale}:
             print('Codice prodotto:', prodotto)
-            print('Rivenditore ufficiale:', ufficiale[0])
+            print('Rivenditore ufficiale:', ufficiale)
             print('Lista rivenditori:', end=' ')
             for rivenditore in rivenditori:
                 print(rivenditore, end=' ')
